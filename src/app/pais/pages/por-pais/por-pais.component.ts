aa945bf4c7dbf9209904998e5ccaf8626a51e474
Yhomarth Reyes <yhomarth@gmail.com>
2021-08-15 19:35:38 -0400

    clase 107 completada, llenar la tabla de países

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -11,6 +12,7 @@ export class PorPaisComponent{
 
   termino: string = '';
   hayError: boolean = false;
+  paises : Country[] = [];
 
   constructor( private paisService: PaisService  ) { }
 
@@ -20,11 +22,13 @@ export class PorPaisComponent{
     console.log(this.termino);
     
     this.paisService.buscarPais( this.termino  ).subscribe (
-      (resp) => {
-        console.log(resp);
+      (paises) => {
+        console.log(paises);
+        this.paises = paises;
       }, 
       (err) => {
         this.hayError = true;
+        this.paises = [];
       }
     )
 
