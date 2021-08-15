f6ec7055b790f89ed4ad9b44e40e2671df46eb0a
Yhomarth Reyes <yhomarth@gmail.com>
2021-08-15 19:02:00 -0400

    clase 105 completada, Manejo de errores

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+//import { Observable, of } from 'rxjs';
+// import { catchError } from 'rxjs/operators'
 
 
 @Injectable({
@@ -14,7 +16,9 @@ export class PaisService {
 
   buscarPais( termino: string ) :Observable<any> {
     const url: string = `${this.urlApi}/name/${termino}`;
-    return this.http.get(  url   );
+    // return this.http.get(  url   ).pipe( catchError(  (err)=> of( [ ]  )  ));
+
+    return this.http.get(url);
 
 
   }
